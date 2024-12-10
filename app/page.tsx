21aa1c54917f81dcfe3f5253b614da8202dbfcad
RanVix <maksim08k@gmail.com>
2024-12-10 17:32:58 +0300

    Added a profile page + its functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,128 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
+import UnknowAva from "../public/assets/DefAva.png";
 
-export default function Home() {
+const ProfilePage: React.FC = () => {
+  const [avatar, setAvatar] = useState<File | null>(null);
+  const [username, setUsername] = useState("Неизвестное имя");
+  const [nickname, setNickname] = useState("Неизвестный ник");
+  const [role, setRole] = useState("Ученик");
+  const [isEditing, setIsEditing] = useState(false);
+  const [portfolio, setPortfolio] = useState<File | null>(null);
+
+  const handleAvatarChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      setAvatar(event.target.files[0]);
+    }
+  };
+
+  const handlePortfolioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      setPortfolio(event.target.files[0]);
+    }
+  };
+
+  const handleEditToggle = () => {
+    setIsEditing(!isEditing);
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div className="flex">
+      <div className="w-[310px] h-screen bg-[#606C38] p-4">
+        <h2 className="text-white text-xl">Меню</h2>
+      </div>
+      <div className="ml-[250px] p-8 w-full">
+        <div className="flex flex-col items-start">
+          <div className="relative w-[325px] h-[325px] mb-4">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={avatar ? URL.createObjectURL(avatar) : UnknowAva}
+              alt="Avatar"
+              layout="fill"
+              objectFit="cover"
+              className="rounded-full cursor-pointer transition-transform transform duration-200 hover:scale-105 hover:blur ml-[-10px]"
+              onClick={() => document.getElementById('avatar-input')?.click()}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            <input
+              type="file"
+              id="avatar-input"
+              accept="image/*"
+              onChange={handleAvatarChange}
+              className="hidden"
+            />
+          </div>
+          {isEditing ? (
+            <>
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="border p-2 w-[250px] rounded-lg mb-2"
+                placeholder="Введите имя пользователя"
+              />
+              <input
+                type="text"
+                value={nickname}
+                onChange={(e) => setNickname(e.target.value)}
+                className="border p-2 w-[250px] rounded-lg mb-2"
+                placeholder="Введите ник"
+              />
+              <select
+                value={role}
+                onChange={(e) => setRole(e.target.value)}
+                className="border p-2 w-[250px] rounded-lg mb-4"
+              >
+                <option value="Ученик">Ученик</option>
+                <option value="Учитель">Учитель</option>
+                <option value="Родитель">Родитель</option>
+              </select>
+            </>
+          ) : (
+            <>
+              <p className="border p-2 w-[250px] rounded-lg mb-2 text-left">{username}</p>
+              <p className="border p-2 w-[250px] rounded-lg mb-2 text-left">{nickname}</p>
+              <p className="border p-2 w-[250px] rounded-lg mb-4 text-left">{role}</p>
+            </>
+          )}
+          <button
+            className="w-[300px] bg-white border border-[#606C38] text-[#606C38] py-2 rounded-lg hover:bg-[#606C38] hover:text-white transition-colors mb-4"
+            onClick={handleEditToggle}
           >
-            Read our docs
-          </a>
+            {isEditing ? "Сохранить настройки" : "Редактировать"}
+          </button>
+          {/* Блок портфолио в правом верхнем углу */}
+          <div className="absolute right-40 w-400">
+            <h3 className="text-lg">Портфолио:</h3>
+            <div className="relative">
+              {!portfolio ? (
+                <>
+                  <p className="text-gray-600">Вы еще не загрузили портфолио.</p>
+                  <input
+                    type="file"
+                    accept=".pdf"
+                    onChange={handlePortfolioChange}
+                    className="mt-2"
+                  />
+                </>
+              ) : (
+                <>
+                  <a
+                    href={URL.createObjectURL(portfolio)}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-blue-500"
+                  >
+                    {portfolio.name}
+                  </a>
+                </>
+              )}
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <p className="mt-4 text-center">Связаться для трудоустройства</p>
+      </div>
     </div>
   );
-}
+};
+
+export default ProfilePage;
